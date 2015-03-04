4e0b01b105ecd27236cfe618c32c0fd36b66a9d4
Dan Wahlin <dwahlin@xmlforasp.net>
2015-03-03 17:12:09 -0700

    Updated customers view, directive, and minor changes in index.html.

---
+++ src/app/directives/filterTextbox.directive.ts
@@ -0,0 +1,39 @@
+///<reference path="../../../tools/typings/tsd.d.ts" />
+///<reference path="../../../tools/typings/typescriptApp.d.ts" />
+
+module demoApp.directives {
+
+    class FilterTextbox implements ng.IDirective {
+
+        static instance() : ng.IDirective {
+            return new FilterTextbox();
+        }
+
+        template = 'Search: <input type="text" ng-model="vm.filter" /> {{ vm.message }}';
+        restrict = 'E';
+        scope = {
+            filter: '='
+        };
+        controller: ($scope: ng.IScope) => void;
+        controllerAs = 'vm';
+        bindToController = true;
+
+        constructor() {
+            this.controller = function ($scope: ng.IScope) {
+                var vm = this;
+                vm.message = 'Hello';
+
+                $scope.$watch('vm.filter', (newVal, oldVal) => {
+                    if (oldVal !== '' && newVal === '') {
+                        vm.message = 'Please enter a value';
+                    } else {
+                        vm.message = '';
+                    }
+                });
+            };
+        }
+    }
+
+    angular.module('demoApp').directive('filterTextbox', FilterTextbox.instance);
+
+}
