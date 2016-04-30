309ea114923dfc954363973a29236f2662bf267f
Dan Wahlin <dwahlin@xmlforasp.net>
2016-04-30 13:10:48 -0700

    Moved from TSD to Typings

---
+++ typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/angular-animate/index.d.ts" />
+/// <reference path="browser/ambient/angular-route/index.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main/ambient/angular-animate/index.d.ts" />
+/// <reference path="main/ambient/angular-route/index.d.ts" />
+/// <reference path="main/ambient/angular/index.d.ts" />
+/// <reference path="main/ambient/jquery/index.d.ts" />
